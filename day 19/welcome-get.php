c7e43311b0c68e4d266adc7f94a339f32ca5f34b
Aryan jadav <137373764+arynn18@users.noreply.github.com>
2023-07-10 15:32:06 +0530

    Add files via upload

---
+++ day 19/welcome-get.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome.php" method="GET">
+        name:
+        <input type="text" name="txt1" placeholder="enter the name"><br>
+        email:
+        <input type="text" name="email" placeholder="enter your email"><br>
+        <input type="submit" value="submit">
+    </form>   
+</body>
+</html>
---
+++ day 19/welcome.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "welcome:"." ";
+    echo $_GET["txt1"];
+
+    echo "your email address is:"." ";
+    echo $_GET["email"];
+    ?>
+</body>
+</html>
